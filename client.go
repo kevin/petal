eea1bf4c868f79bdde4af2422328e14f63e0dd7d
kevin <35074883+kevin@users.noreply.github.com>
2024-08-14 17:39:11 -0500

    basic client and config structs

--- client.go
+++ client.go
@@ -1,8 +1,27 @@
 package petal
 
 type Client struct {
+	AppKey      string
+	AppSecret   string
+	CallbackURL string
 }
 
-func NewClient() (*Client, error) {
-	return &Client{}, nil
+func NewClient(config *Config) (*Client, error) {
+	client := &Client{
+		AppKey:      config.AppKey,
+		AppSecret:   config.AppSecret,
+		CallbackURL: config.CallbackURL,
+	}
+
+	return client, nil
+}
+
+type Config struct {
+	AppKey      string
+	AppSecret   string
+	CallbackURL string
+}
+
+func NewConfig(appKey, appSecret, callbackURl string) *Config {
+	return &Config{AppKey: appKey, AppSecret: appSecret, CallbackURL: callbackURl}
 }
